6cd44fd1f5485e10d669c77c857332faf6ff4d20
Winfried Baumann <frieder@holoride.com>
2022-11-23 15:06:31 +0100

    reformatted

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
-use std::net::TcpListener;
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(address: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-        })
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(address)?
         .run();
 
