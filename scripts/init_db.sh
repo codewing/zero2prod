fef8c4c762ba79dcff94ea4e6adcc90c4c75c4e3
Winfried Baumann <frieder@holoride.com>
2022-11-24 12:23:00 +0100

    fixed init script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v psql)" ]; then
   echo >&2 "Error: psql is not installed."
   exit 1
 fi
-if ! [ -x "$(command -v cargo sqlx)" ]; then
+if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
   echo >&2 " cargo install --version='~0.6' sqlx-cli \
