326424ea50d0d25d8d8feda015604a965617689f
Winfried Baumann <codewing@web.de>
2022-12-18 20:55:32 +0100

    resetting the docker instance if setup was already executed

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -26,22 +26,31 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=my_very_secure_database_password_1}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
+DOCKER_POSTGRES_NAME=${DOCKER_POSTGRES_CONTAINER:=zero2prod_db}
+
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 export DATABASE_URL=${DATABASE_URL}
 
 # Launch postgres using Docker
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  docker run \
-    --name="zero2prod_db" \
+  if [ ! "$(docker ps -q -f name=${DOCKER_POSTGRES_NAME})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${DOCKER_POSTGRES_NAME})" ]; then
+        # cleanup
+        docker rm ${DOCKER_POSTGRES_NAME}
+    fi
+    
+    docker run \
+    --name=${DOCKER_POSTGRES_NAME} \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
+    # ^ Increased maximum number of connections for testing purposes
+  fi
 fi
-# ^ Increased maximum number of connections for testing purposes
 
 # Keep pinging Postgres until it's ready to accept commands
 until PGPASSWORD="${DB_PASSWORD}" psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
